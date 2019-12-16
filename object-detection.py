7bb3f2e0118a60fab4a644ea121e737113b7cce2
neemiasbsilva <neemiasbsilva@gmail.com>
2019-12-16 17:56:19 -0400

    Draw the rectangle to the object (Dog)

--- object-detection.py
+++ object-detection.py
@@ -22,7 +22,21 @@ def detect(frame, net, transform):
     x = Variable(x.unsqueeze(0))
     y = net(x)
     
-    # Next step
+    # Next step Tensor and data
     detections = y.data
     scale = torch.Tensor([width, height, width, height])
     
+    # detections = [batch, number of classes, number of ocurrence, [score, x0, y0, x1, y1]]
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= 0.6:
+            pt = (detections[0, i, j, 1:] * scale).numpy()
+
+            # using openCV to draw the rectangle
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])) , (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
+            cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])),
+                cv2.FONT_HERSHEY_SIMPLEY, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
+
+    return frame
+
