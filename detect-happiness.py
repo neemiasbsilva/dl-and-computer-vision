96435aa7c206e44a3b1284b3fbf7abdb72807b29
neemiasbsilva <neemiasbsilva@gmail.com>
2019-12-15 20:37:37 -0400

    Implementation the smile detector

--- detect-happiness.py
+++ detect-happiness.py
@@ -2,3 +2,43 @@
 import cv2
 
 # include the desired haar-cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+# check for smile
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), ((x+w), (y+h)), (0, 0, 255), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
+
+        for (sx, sy, sw, sh) in faces:
+            cv2.rectangle(frame, (sx, sy), ((sx+sw), (sy+sh)), (255, 0, 0), 2)
+
+    return frame    
+
+# define main function
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Captures video_capture frame by frame
+    _, frame = video_capture.read()
+
+    # To capture image in monochrome
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # call the detect() function
+    canvas = detect(gray, frame)
+
+    # Display the result on camera feed
+    cv2.imshow('Video', canvas)
+
+    # The control breaks once q key is pressed
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
