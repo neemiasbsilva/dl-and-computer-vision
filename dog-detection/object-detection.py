6f7215666093b0240845953759a2dd887e5cea99
neemiasbsilva <neemiasbsilva@gmail.com>
2020-01-14 06:46:19 -0400

    Applie SSD to detect dog & persons

--- dog-detection/object-detection.py
+++ dog-detection/object-detection.py
@@ -51,14 +51,14 @@ transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0))
 # Doing some object detection on a video
 # imageio is the greate to manipulating video
 reader = imageio.get_reader('funny_dog.mp4')
-# fps = reader.get_meta_data()['fps']
-# writer = imageio.get_writer('output.mp4', fps = fps)
+fps = reader.get_meta_data()['fps']
+writer = imageio.get_writer('output.mp4', fps = fps)
 
-# for i, frame in enumerate(reader):
-#     frame = detect(frame, net.eval(), transform)
-#     # Apend to write video
-#     writer.append_data(frame)
-#     print(i)
+for i, frame in enumerate(reader):
+    frame = detect(frame, net.eval(), transform)
+    # Apend to write video
+    writer.append_data(frame)
+    print(i)
 
 # Close the process
-# writer.close()
+writer.close()
